d5d83ac613a35bf4d5b8f02f8863faa5a146d115
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-11-11 12:29:39 -0500

    small changes for master

--- client/app.js
+++ client/app.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {playRandom} from './magenta-random'
 
 const App = () => {
   return (
     <div>
       <h1>BAD MUSICIAN</h1>
+      <button type="button" id="play" onClick={playRandom}>
+        Play random
+      </button>
     </div>
   )
 }
